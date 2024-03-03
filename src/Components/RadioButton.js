bcbe17fcaf96db170f7ac32d1ecaacaf659c35f7
abhinavudavant <75460327+abhinavudavant@users.noreply.github.com>
2024-03-03 09:59:00 +0530

    Update RadioButton.js

--- src/Components/RadioButton.js
+++ src/Components/RadioButton.js
@@ -1,9 +1,7 @@
-// RadioButton component
-
 import React from 'react';
 
 const RadioButton = ({ label, value, name, checked, onChange, className }) => {
-  // Ensure that the value is not null
+
   const sanitizedValue = value === null ? '' : value;
 
   return (
