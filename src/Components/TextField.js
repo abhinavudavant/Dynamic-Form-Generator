09355f8335007fafab97b42e754f8f7eea9a27a1
abhinavudavant <75460327+abhinavudavant@users.noreply.github.com>
2024-03-03 09:57:50 +0530

    Create TextField.js

---
+++ src/Components/TextField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TextField = ({ label, value, onChange }) => (
+  <div>
+    <label>{label}</label>
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  </div>
+);
+
+export default TextField;
